075a814570f0647f57bd064f4d28e06149052207
KadyrovErjan <kadyroverjan007@gmail.com>
2026-03-29 15:24:06 +0600

    readme.md changed

--- camera.py
+++ camera.py
@@ -1,6 +1,6 @@
 import cv2
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,  1750)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 700)
 cap.set(cv2.CAP_PROP_FPS,          30)
